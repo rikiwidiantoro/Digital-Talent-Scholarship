135b1da48abe2233b9b362f971c7dd30dab3b795
Riki-Toro <rikitoro22@gmail.com>
2022-03-29 21:38:39 +0700

    update

--- FGA-Frontend Dev Progate 2022/tugas_individu_calculator/js/script.js
+++ FGA-Frontend Dev Progate 2022/tugas_individu_calculator/js/script.js
@@ -1,128 +1,117 @@
-// $(document).ready(function() {
-    // membuat variabel
-    const kalkulator = {
-        angka: '0',
-        operator: null,
-        angkaPertama: null,
-        angkaKedua: false
-    };
+// membuat variabel
+const kalkulator = {
+    angka: '0',
+    operator: null,
+    angkaPertama: null,
+    angkaKedua: false
+};
 
-    // meng-update angka di layar
-    function updateLayar() {
-        document.querySelector(".hasil").innerText = kalkulator.angka;
+// meng-update angka di layar
+function updateLayar() {
+    document.querySelector(".hasil").innerText = kalkulator.angka;
+}
+
+// meng-hapus angka di layar
+function hapusLayar() {
+    kalkulator.angka = '0';
+    kalkulator.operator = null;
+    kalkulator.angkaPertama = null;
+    kalkulator.angkaKedua = false;
+}
+
+function inputAngka(angkaTombol) {
+    if( kalkulator.angka === '0' ) {
+        kalkulator.angka = angkaTombol;
+    } else {
+        kalkulator.angka += angkaTombol;
+    }
+}
+
+// jika negatif angka
+function negatif() {
+    if( kalkulator.angka === '0' ) {
+        return;
     }
 
-    // meng-hapus angka di layar
-    function hapusLayar() {
+    kalkulator.angka = kalkulator.angka * -1;
+}
+
+function operatorlayar(operator) {
+    if( !kalkulator.angkaKedua ) {
+        kalkulator.operator = operator;
+        kalkulator.angkaKedua = true;
+        kalkulator.angkaPertama = kalkulator.angka;
         kalkulator.angka = '0';
-        kalkulator.operator = null;
-        kalkulator.angkaPertama = null;
-        kalkulator.angkaKedua = false;
+    } else {
+        alert("Operator sudah ditetapkan");
+    }
+}
+
+function samadengan() {
+    if( kalkulator.angkaPertama == null || kalkulator.operator == null ) {
+        alert("Anda belum menetapkan operator");
+        return;
     }
 
-    function inputAngka(angkaTombol) {
-        if( kalkulator.angka === '0' ) {
-            kalkulator.angka = angkaTombol;
-        } else {
-            kalkulator.angka += angkaTombol;
-        }
+    let hasil = 0;
+    if( kalkulator.operator === "+" ) {
+        hasil = parseInt(kalkulator.angkaPertama) + parseInt(kalkulator.angka);
+    } else if( kalkulator.operator === "-" ) {
+        hasil = parseInt(kalkulator.angkaPertama) - parseInt(kalkulator.angka);
+    } else if( kalkulator.operator === "*" ) {
+        hasil = parseInt(kalkulator.angkaPertama) * parseInt(kalkulator.angka);
+    } else if( kalkulator.operator === "/" ) {
+        hasil = parseInt(kalkulator.angkaPertama) / parseInt(kalkulator.angka);
     }
 
-    // jika negatif angka
-    function negatif() {
-        if( kalkulator.angka === '0' ) {
-            return;
-        }
-
-        kalkulator.angka = kalkulator.angka * -1;
+    // membuat object history
+    const history = {
+        angkaPertama: kalkulator.angkaPertama,
+        angkaKedua: kalkulator.angka,
+        operator: kalkulator.operator,
+        hasil: hasil
     }
+    ambilHistory(history);
 
-    function operatorlayar(operator) {
-        if( !kalkulator.angkaKedua ) {
-            kalkulator.operator = operator;
-            kalkulator.angkaKedua = true;
-            kalkulator.angkaPertama = kalkulator.angka;
-            kalkulator.angka = '0';
-        } else {
-            alert("Operator sudah ditetapkan");
-        }
-    }
+    kalkulator.angka = hasil;
 
-    function samadengan() {
-        if( kalkulator.angkaPertama == null || kalkulator.operator == null ) {
-            alert("Anda belum menetapkan operator");
-            return;
-        }
-
-        let hasil = 0;
-        if( kalkulator.operator === "+" ) {
-            hasil = parseInt(kalkulator.angkaPertama) + parseInt(kalkulator.angka);
-        } else if( kalkulator.operator === "-" ) {
-            hasil = parseInt(kalkulator.angkaPertama) - parseInt(kalkulator.angka);
-        } else if( kalkulator.operator === "*" ) {
-            hasil = parseInt(kalkulator.angkaPertama) * parseInt(kalkulator.angka);
-        } else if( kalkulator.operator === "/" ) {
-            hasil = parseInt(kalkulator.angkaPertama) / parseInt(kalkulator.angka);
-        }
-
-        // membuat object history
-        const history = {
-            angkaPertama: kalkulator.angkaPertama,
-            angkaKedua: kalkulator.angka,
-            operator: kalkulator.operator,
-            hasil: hasil
-        }
-        ambilHistory(history);
-
-        kalkulator.angka = hasil;
-
-        renderHistory();
-    }
+    renderHistory();
+}
 
 
 
-
-
-
-
-    // mendefinisikan semua variabel tombol
-    const tombolAngka = document.querySelectorAll(".tombol");
-    // perulangan untuk 1 tombol
-    for( tombol of tombolAngka ) {
-        tombol.addEventListener('click', function(event) {
-            const target = event.target; //target tombol
-            
-            // pengkondisian jika melakukan operasi
-            if( target.classList.contains('hapus') ) {
-                hapusLayar();
-                updateLayar();
-                return;
-            }
-            if( target.classList.contains('negatif') ) {
-                negatif();
-                updateLayar();
-                return;
-            }
-            if( target.classList.contains('samadengan') ) {
-                samadengan();
-                updateLayar();
-                return;
-            }
-            if( target.classList.contains('operator') ) {
-                operatorlayar(target.innerText);
-                return;
-            }
-
-            inputAngka(target.innerText);
+// mendefinisikan semua variabel tombol
+const tombolAngka = document.querySelectorAll(".tombol");
+// perulangan untuk 1 tombol
+for( tombol of tombolAngka ) {
+    tombol.addEventListener('click', function(event) {
+        const target = event.target; //target tombol
+        
+        // pengkondisian jika melakukan operasi
+        if( target.classList.contains('hapus') ) {
+            hapusLayar();
             updateLayar();
-        });
+            return;
+        }
+        if( target.classList.contains('negatif') ) {
+            negatif();
+            updateLayar();
+            return;
+        }
+        if( target.classList.contains('samadengan') ) {
+            samadengan();
+            updateLayar();
+            return;
+        }
+        if( target.classList.contains('operator') ) {
+            operatorlayar(target.innerText);
+            return;
+        }
+
+        inputAngka(target.innerText);
+        updateLayar();
+    });
 
 
-    }
+}
 
-
-
-    // updateLayar();
-    // inputAngka();
-    // console.log(kalkulator);
-// });
--- FGA-Frontend Dev Progate 2022/tugas_individu_calculator/js/storage.js
+++ FGA-Frontend Dev Progate 2022/tugas_individu_calculator/js/storage.js
@@ -1,70 +1,63 @@
-// $(document).ready(function() {
-    // kenapa jquery saya komentari? karena tidak bisa mentransfer function di luar file
-    
-    // membuat kunci/key
-    const KEY = "calculator_history";
+// membuat kunci/key
+const KEY = "calculator_history";
 
-    // mengecek apakah storage ada isinya
-    function cekStorage() {
-        return typeof(Storage) !== "undefined";
-        // typeof(Storage);
-    }
+// mengecek apakah storage ada isinya
+function cekStorage() {
+    return typeof(Storage) !== "undefined";
+    // typeof(Storage);
+}
 
 
-    function ambilHistory(data) {
-        if( cekStorage() ) {
-            let dataHistory = null;
+function ambilHistory(data) {
+    if( cekStorage() ) {
+        let dataHistory = null;
 
-            // getItem(key)
-            if( localStorage.getItem(KEY) === null ) {
-                dataHistory = [];
-            } else {
-                dataHistory = JSON.parse(localStorage.getItem(KEY));
-            }
-
-            dataHistory.unshift(data);
-
-            // jika data history lebih dari 5 maka ambil yang 5 terbaru
-            if( dataHistory.length > 5 ) {
-                dataHistory.pop();
-            }
-
-            // setItem(key, value)
-            localStorage.setItem(KEY, JSON.stringify(dataHistory));
-        }
-    }
-    // ambilHistory(history);
-
-    function tampilHistory() {
-        if( cekStorage() ) {
-            return JSON.parse(localStorage.getItem(KEY)) || [];
+        // getItem(key)
+        if( localStorage.getItem(KEY) === null ) {
+            dataHistory = [];
         } else {
-            return [];
+            dataHistory = JSON.parse(localStorage.getItem(KEY));
         }
-    }
 
+        dataHistory.unshift(data);
 
-    function renderHistory() {
-        // mengambil data history dari tampilData
-        const historyData = tampilHistory();
-        let historyList = document.getElementById('historyList');
-
-        historyList.innerHTML = "";
-
-        // pengulangan untuk tabel
-        for( let history of historyData ) {
-            let row = document.createElement('tr');
-            
-            row.innerHTML = `<td> ${history.angkaPertama} </td>`;
-            row.innerHTML += `<td> ${history.operator} </td>`;
-            row.innerHTML += `<td> ${history.angkaKedua} </td>`;
-            row.innerHTML += `<td> ${history.hasil} </td>`;
-
-            historyList.appendChild(row);
+        // jika data history lebih dari 5 maka ambil yang 5 terbaru
+        if( dataHistory.length > 5 ) {
+            dataHistory.pop();
         }
+
+        // setItem(key, value)
+        localStorage.setItem(KEY, JSON.stringify(dataHistory));
     }
-    renderHistory();
+}
 
-    // console.log(tampilHistory());
 
-// });
+function tampilHistory() {
+    if( cekStorage() ) {
+        return JSON.parse(localStorage.getItem(KEY)) || [];
+    } else {
+        return [];
+    }
+}
+
+
+function renderHistory() {
+    // mengambil data history dari tampilData
+    const historyData = tampilHistory();
+    let historyList = document.getElementById('historyList');
+
+    historyList.innerHTML = "";
+
+    // pengulangan untuk tabel
+    for( let history of historyData ) {
+        let row = document.createElement('tr');
+        
+        row.innerHTML = `<td> ${history.angkaPertama} </td>`;
+        row.innerHTML += `<td> ${history.operator} </td>`;
+        row.innerHTML += `<td> ${history.angkaKedua} </td>`;
+        row.innerHTML += `<td> ${history.hasil} </td>`;
+
+        historyList.appendChild(row);
+    }
+}
+renderHistory();
