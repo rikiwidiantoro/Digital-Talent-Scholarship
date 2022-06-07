934dffcc717856bc9213aaf6b389c0d3e048f01f
Riki Widiantoro <rikitoro22@gmail.com>
2022-06-07 19:07:20 +0700

    file js

---
+++ FGA-Javascript with API Progate 2022/Javascript/javascript 3/page2/script.js
@@ -0,0 +1,9 @@
+// Tetapkan function ke constant hello
+const hello = function() {
+  console.log("Halo!");
+  console.log("Saya Riki Widiantoro");
+}
+
+
+// Panggil function yang ditetapkan di constant hello
+hello();
---
+++ FGA-Javascript with API Progate 2022/Javascript/javascript 3/page9/script.js
@@ -0,0 +1,11 @@
+const number1 = 103;
+const number2 = 72;
+const number3 = 189;
+
+// Ketik sebuah function getMax untuk mendapatkan nilai maksimum
+const getMax = (a, b, c) => {
+  return a, b, c;
+}
+
+// Print "Nilai maksimum adalah __"
+console.log(`Nilai maksimum adalah ${getMax(number1, number2, number3)}`);
